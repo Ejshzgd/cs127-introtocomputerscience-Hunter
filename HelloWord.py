35393469ea946ddd3c25dc2c20562b8968b54888
Your Name <careyj2024@gmail.com>
2024-09-16 22:21:21 -0400

    New Assingment Added

--- HelloWord.py
+++ HelloWord.py
@@ -11,3 +11,4 @@ for i in range(4):
   thomasH.forward(100)  
   thomasH.left(90)      
 
+thomasH.screen.exitOnClick()
---
+++ printThoseNum.py
@@ -0,0 +1,7 @@
+#Name: Carey Jiang
+#Email: carey.jiang65@myhunter.cuny.edu
+#Date: September 16, 2024
+#This program prints numbers from 0 - 14
+
+for i in range(15):
+    print(i)
