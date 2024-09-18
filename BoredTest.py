a179d28bdcdd232517f5b33905836a80ad8b6e0d
Ejshzgd <2020caji5602@gmail.com>
2024-09-18 10:26:07 -0400

    newFile

---
+++ BoredTest.py
@@ -0,0 +1,28 @@
+# l1 = ["eat", "sleep", "repeat"]
+
+# # printing the tuples in object directly
+# for ele in enumerate("Carey"):
+#     print (ele)
+
+# # # changing index and printing separately
+# # for count, ele in enumerate(l1, 100):
+# #     print (count, ele)
+
+# # # getting desired output from tuple
+# # for count, ele in enumerate(l1):
+# #     print(count)
+# #     print(ele)
+
+
+# # ///print out 2 characters, compare the unicode, print out the bigger one
+
+# myString = input("Please enter a String ")
+
+# for i in range (len(myString)-1):
+#     stringOne = myString[i : i+1]
+#     stringTwo = myString[i+1 : i+2]
+
+#     if ord(stringOne[0]) > ord(stringTwo[0]):
+#         print(ord(stringOne[0]) , stringOne)
+#     else:
+#         print(ord(stringTwo[0]) , stringTwo)
