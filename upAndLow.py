dc49d209d3e84ddb85aa004ca6844f799e734045
Ejshzgd <2020caji5602@gmail.com>
2024-09-19 17:14:31 -0400

    newHomework

---
+++ upAndLow.py
@@ -0,0 +1,9 @@
+#Name: Carey Jiang
+#Email: carey.jiang65@myhunter.cuny.edu
+#Date: September 19, 2024
+#This program stores user input and print it three times: Original, Uppercase, and Lowercase
+
+messAge = input("Enter a message:  ")
+print(messAge)
+print(messAge.upper())
+print(messAge.lower())
