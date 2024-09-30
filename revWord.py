501e0c5cb9abcdb681c636714945369752403685
Carey <153955001+Ejshzgd@users.noreply.github.com>
2024-09-30 16:33:21 -0400

    Update revWord.py

--- revWord.py
+++ revWord.py
@@ -8,7 +8,7 @@
 # for i in range(1 , len(ogWord)):
 #     print(ogWord[-i-1 : -i] , end=" ")
 
-ogWord = input("Enter a message: ")
+ogWord = input("Enter a message:")
 
 for i in range(len(ogWord) , 0 , -1):
     revWord = ogWord[i-1:i]
